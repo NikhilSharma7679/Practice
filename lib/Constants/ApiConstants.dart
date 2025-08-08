d73e31c80a3b025c12fc5f0aac0c0e0ab1919101
KinCharvi Innovation <nikhilsharma7679@gmail.com>
2025-08-08 10:53:45 +0530

    2nd time

---
+++ lib/Constants/ApiConstants.dart
@@ -0,0 +1,12 @@
+class ApiConstants{
+  static const String BASE_URL = "http://admin.astrohut.in/api";
+  static const String Ext_APi_key = '4a4d7afc-665b-5b3b-a671-2ff3e4af2ff6';
+  static const String Ext_API_BASEURL = "https://api.vedicastroapi.com/v3-json";
+
+
+  //APis
+  static const String Login = "/login";
+
+
+
+}
