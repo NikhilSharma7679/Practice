e96f58598d15d56a86d90f9e6ea4d1b2e9718f20
hp <pannu.hunny123@gmail.com>
2025-08-10 10:05:27 +0530

    initial commit

--- lib/Ui/LoginView/LoginView.dart
+++ lib/Ui/LoginView/LoginView.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // For social icons
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Loginview extends StatefulWidget {
   const Loginview({Key? key}) : super(key: key);
