833bee18c11edcb0f68f7ced712a1cefccd581ef
hp <pannu.hunny123@gmail.com>
2025-09-06 13:56:29 +0530

    player home

--- lib/Ui/PlayerUI/SettingsPageView/SettingsPageView.dart
+++ lib/Ui/PlayerUI/SettingsPageView/SettingsPageView.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../TrainingPageview/TraingPageview.dart';
 import '../DiscoverPageView/DiscoverPageView.dart';
 import '../ReportPageView/ReportPageView.dart';
 
@@ -18,12 +17,6 @@ class _SettingspageviewState extends State<Settingspageview> {
     if (index == _selectedIndex) return;
 
     switch (index) {
-      case 0:
-        Navigator.pushReplacement(
-          context,
-          // MaterialPageRoute(builder: (context) => TrainingPageView()),
-        );
-        break;
       case 1:
         Navigator.pushReplacement(
           context,
